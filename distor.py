38c53ae60b09b66d1923242f8e3256b289f100ca
Ahacad <1393345075@qq.com>
2020-06-26 17:59:13 +0800

    Finish print and data management sections

--- distor.py
+++ distor.py
@@ -1,11 +1,49 @@
+# /usr/bin/env python3
+
 import os
+import json
+
+sections = {}
+sections["sections"] = []
+storedPath = "/home/ahacad/.distor/stored.json"
 
 
-def addEntry():
-    """add an entry to the existing pool
+class colors:
+    RED   = "\033[1;31m"  
+    BLUE  = "\033[1;34m"
+    CYAN  = "\033[1;36m"
+    GREEN = "\033[0;32m"
+    RESET = "\033[0;0m"
+    BOLD    = "\033[;1m"
+    REVERSE = "\033[;7m""]"
 
+
+def loadSection():
+    """load sections from the stored json file
     """
-    pass
+    with open(storedPath) as json_file:
+        sections = json.load(json_file)
+    return sections
+
+
+def writeEntry():
+    """store entries to the path
+    """
+    with open(storedPath, "w") as outfile:
+        json.dump(sections, outfile)
+
+
+def addEntry(name, project, timeEstimation, DDL, remarks):
+    """add an entry to the existing pool
+    """
+    sections["sections"].append({
+        "Name": name,
+        "Project": project,
+        "Time Estimation": timeEstimation,
+        "DDL": DDL,
+        "Remarks": remarks
+        })
+    return 0
 
 
 def deprecateEntry():
@@ -18,7 +56,19 @@ def deprecateEntry():
 def printEntry():
     """print the entries in the existing pool, you can choose the way to display
     """
-    pass
+    width = 20
+    for section in sections["sections"]:
+        name = section["Name"]
+        project = section["Project"]
+        timeEstimation = section["Time Estimation"]
+        DDL = section["DDL"]
+        remarks = section["Remarks"]
+        print(f"{name:^{width}}|" +
+              f"{project:^{width}}|" +
+              f"{timeEstimation:^{width}}|" +
+              f"{DDL:^{width}}|" +
+              f"{remarks:^{width}}")
+    return 0
 
 
 def estimateTime():
@@ -28,9 +78,28 @@ def estimateTime():
     pass
 
 
+def cli():
+    """the command line interface
+
+    """
+    pass
+
+
 def main():
+    global sections
+    # sections = loadSection()
+    # addEntry(name, project, timeEstimation, DDL, remarks)
+    sections = {"sections": [{"Name": "testname1", "Project": "Mathe", "Time Estimation": "2", "DDL": "2020-06-12", "Remarks": "test"}]}
+    printEntry()
+    writeEntry()
     pass
 
 
 if __name__ == "__main__":
     main()
+
+
+
+# techniques used:
+# - json
+# - format alignment and colored printing
