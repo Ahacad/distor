224516644924b76767271dc034e055a4ca518a22
Ahacad <1393345075@qq.com>
2020-06-26 16:35:48 +0800

    Initial commit

---
+++ distor.py
@@ -0,0 +1,36 @@
+import os
+
+
+def addEntry():
+    """add an entry to the existing pool
+
+    """
+    pass
+
+
+def deprecateEntry():
+    """remove the entry from the existing pool and add to the deprecated pool
+
+    """
+    pass
+
+
+def printEntry():
+    """print the entries in the existing pool, you can choose the way to display
+    """
+    pass
+
+
+def estimateTime():
+    # how the time remained is calculated?
+    """estimate whether you can make it before ddl
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
